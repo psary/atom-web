3ad7af9e3ffc24fa1b2b767e8a3513cae14a9b9f
Anthony Collin <4nthony.collin@gmail.com>
2022-03-01 00:25:00 +0100

    update

--- client/main.lua
+++ client/main.lua
@@ -7,7 +7,7 @@ AddEventHandler("atom-web:show-board", function(data)
     SetNuiFocus(false,false)
     local event={}
     event.data = data
-    event.type = open
+    event.type = "open"
 	SendNUIMessage(event)
 end)
 
@@ -15,6 +15,6 @@ RegisterNetEvent("atom-web:close-board")
 AddEventHandler("atom-web:close-board", function()
     SetNuiFocus(false,false)
     local event={}
-    event.type = open
+    event.type = "close"
 	SendNUIMessage(event)
 end)
--- html/main/assets/js/script.js
+++ html/main/assets/js/script.js
@@ -3,7 +3,7 @@ $('document').ready(function(){
     window.addEventListener("message", (event) => {
         if(event.data.type === "open"){
             $("#video").css("display", "flex") 
-            $.each(event.data, function( index, value ) {
+            $.each(event.data.data, function( index, value ) {
                 $("#video").append("<div class=\"message\"><div class=\"title\">"+value.title+"</div><div class=\"content\">"+value.description+"</div><div class=\"author\">"+value.author+"</div>")
               });
         } else {
