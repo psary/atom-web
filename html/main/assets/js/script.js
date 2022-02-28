023560909a3210c8c217f5b572186767aa54a2f8
Anthony Collin <4nthony.collin@gmail.com>
2022-02-28 23:46:00 +0100

    update

--- html/main/assets/js/script.js
+++ html/main/assets/js/script.js
@@ -3,7 +3,7 @@ $('document').ready(function(){
     window.addEventListener("message", (event) => {
         $("#video").css("display", "flex") 
             
-        $("#video").append(JSON.stringify(event))
+        $("#video").append(JSON.stringify(event.data))
     });
 
   
