72e7702a5abcb7f52a8b370208d2a95344346874
Anthony Collin <4nthony.collin@gmail.com>
2022-03-01 00:03:53 +0100

    update

--- html/main/assets/js/script.js
+++ html/main/assets/js/script.js
@@ -3,7 +3,7 @@ $('document').ready(function(){
     window.addEventListener("message", (event) => {
         $("#video").css("display", "flex") 
         $.each(event.data, function( index, value ) {
-            $("#video").append("<div class=\"message\"><div class=\"title\">"+value.title+"</div><div class=\"message\">"+value.description+"</div><div class=\"author\">"+value.author+"</div>")
+            $("#video").append("<div class=\"message\"><div class=\"title\">"+value.title+"</div><div class=\"content\">"+value.description+"</div><div class=\"author\">"+value.author+"</div>")
           });
     });
 
