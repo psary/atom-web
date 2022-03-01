7bcccc721cc9e11ca3985b1576092966d9ec0884
Anthony Collin <4nthony.collin@gmail.com>
2022-03-01 15:02:07 +0100

    update

--- html/main/assets/js/script.js
+++ html/main/assets/js/script.js
@@ -2,16 +2,12 @@ $('document').ready(function(){
   
     window.addEventListener("message", (event) => {
         if(event.data.type === "open"){
-            $("#video").css("display", "flex") 
+            $("#board").css("display", "flex") 
             $.each(event.data.data, function( index, value ) {
-                $("#video").append("<div class=\"message\"><div class=\"title\">"+value.title+"</div><div class=\"content\">"+value.description+"</div><div class=\"author\">"+value.author+"</div>")
-              });
-            
-              $("document").keyup(function(e) {
-                $("#video").css("display", "none");
+                $("#board").append("<div class=\"message\"><div class=\"title\">"+value.title+"</div><div class=\"content\">"+value.description+"</div><div class=\"author\">"+value.author+"</div>")
               });
         } else {
-            $("#video").css("display", "none") 
+            $("#board").css("display", "none") 
         }
     });
 
