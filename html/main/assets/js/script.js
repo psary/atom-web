781e823bef4e5dc081667cc5e8814227ea923e12
Anthony Collin <4nthony.collin@gmail.com>
2022-03-03 16:01:43 +0100

    Update script.js

--- html/main/assets/js/script.js
+++ html/main/assets/js/script.js
@@ -2,11 +2,12 @@ $('document').ready(function(){
   
     window.addEventListener("message", (event) => {
         if(event.data.type === "open"){
-            $("#board").css("display", "flex") 
+            $("#board").html("");
             $.each(event.data.data, function( index, value ) {
                 const type = value.title.charCodeAt(0) % 4;
                 $("#board").append("<div class=\"message-container\"><div class=\"message"+type+"\"><div class=\"title\">"+value.title+"</div><div class=\"content\">"+value.description+"</div><div class=\"author\">"+value.author+"</div></div></div>")
               });
+            $("#board").css("display", "flex") 
         } else {
             $("#board").css("display", "none") 
         }
