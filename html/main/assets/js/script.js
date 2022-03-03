2fcc6f404ba2d9750b393776d31972cf232cbab7
Anthony Collin <4nthony.collin@gmail.com>
2022-03-03 16:25:50 +0100

    update

--- html/main/assets/js/script.js
+++ html/main/assets/js/script.js
@@ -2,14 +2,14 @@ $('document').ready(function(){
   
     window.addEventListener("message", (event) => {
         if(event.data.type === "open"){
-            $("#board").html("");
+            $("#board-container").html("");
             $.each(event.data.data, function( index, value ) {
                 const type = value.title.charCodeAt(0) % 4;
                 $("#board").append("<div class=\"message-container\"><div class=\"message"+type+"\"><div class=\"title\">"+value.title+"</div><div class=\"content\">"+value.description+"</div><div class=\"author\">"+value.author+"</div></div></div>")
               });
-            $("#board").css("display", "flex") 
+            $("#board-container").css("display", "flex") 
         } else {
-            $("#board").css("display", "none") 
+            $("#board-container").css("display", "none") 
         }
     });  
 });
